a73b75eb3ca4921cb891e2f5d0b47f3d3a501437
Ilya Grigorik <ilya@igvita.com>
2011-04-05 00:22:40 -0400

    spec for deflate

--- spec/compressor_spec.rb
+++ spec/compressor_spec.rb
@@ -4,4 +4,11 @@ describe SPDY::Zlib do
   it "should inflate header with custom dictionary" do
     SPDY::Zlib.inflate(COMPRESSED_HEADER).should match('HTTP/1.1')
   end
+
+  it "should deflate header with custom dictionary" do
+    orig = SPDY::Zlib.inflate(COMPRESSED_HEADER)
+    rinse = SPDY::Zlib.inflate(SPDY::Zlib.deflate(orig))
+
+    orig.should == rinse
+  end
 end
